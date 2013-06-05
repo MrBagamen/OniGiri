d5df66170a26223bd63673c93c4f1065f8e21245
MrBagamen <mrbagamen@gmail.com>
2013-06-06 00:29:09 +0200

    check this shet

--- Text.lua
+++ Text.lua
@@ -4,10 +4,11 @@ Text = {
 }
 
 function Text.create()
-  font = love.graphics.newImageFont("res/shitfont.png",
-    " abcdefghijklmnopqrstuvwxyz" ..
-    "ABCDEFGHIJKLMNOPQRSTUVWXYZ0" ..
-    "123456789.,!?-+/():;%&`'*#=[]\"")
+  --font = love.graphics.newImageFont("res/shitfont.png",
+--    " abcdefghijklmnopqrstuvwxyz" ..
+--    "ABCDEFGHIJKLMNOPQRSTUVWXYZ0" ..
+--    "123456789.,!?-+/():;%&`'*#=[]\"")
+  font = love.graphics.newFont("res/BIOST.ttf", 24)
   love.graphics.setFont(font)
   --t1 = love.timer.getTime()
   
@@ -21,11 +22,11 @@ function Text:say(shit)
   for i = 1, #shit do
    -- if love.timer.getTime() - t1 > 0.1 then
       if shit:sub(i, i) == "\n" then
-        self.x = tempx - 10
-        self.y = self.y + 14
+        self.x = tempx - 15
+        self.y = self.y + 20
       end
       love.graphics.print(shit:sub(i, i), self.x, self.y)
-      self.x = self.x + 10
+      self.x = self.x + 15
       --t1 = love.timer.getTime()
     --end
   end  
